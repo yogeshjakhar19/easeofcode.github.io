f8ade5afd383fa351c54b578d4194737a91f99c0
YOGESH JAKHAR <36562190+yogeshjakhar19@users.noreply.github.com>
2018-02-17 15:04:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<! DOCTYPE HTML >
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Let's Learn It</title>
+	<link href="fa/css/font-awesome.min.css " rel="stylesheet" >
+	<link rel="stylesheet" type="text/css" href="mystyle.css">
+	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="HandheldFriendly" content="true">
+</head>
+<body>
+<div id="container">
+    <div id="header">
+    	<div id="top">
+    		<div id="logo">
+    			<img src="images/logo.jpg" alt="EaseOfCode.com" width="auto" height="100px;">
+    		</div>
+    		<div id="search">
+    			<form action="">
+                    <input class="search-area " type="text" name="text" placeholder="search here">
+                    <button class="search-btn" type="submit"><i class="fa fa-search"></i></button>
+                </form>
+    		</div>
+    		<div id="side">
+    			<marquee>Welcome! All Of You ...  Our Website will Help you to " Learn  Good in Short ".</marquee>
+    		</div>
+    	</div>
+    	<div id="bottom">
+    		<ul>
+    			<li><a href="#">Learn C</a></li>
+    			<li><a href="#">Refrences</a></li>
+    			<li><a href="#">Examples</a></li>
+    			<li><a href="#">Books</a></li>
+    			<li><a href="#">Quiz</a></li>
+    			<li><a href="#">About Us</a></li>
+    		</ul>
+    	</div>
+    </div>	
+</div>
+</body>
+</html>
